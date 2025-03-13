d964690de77bfd3c9f31c73da6d8a07b832a520e
Thibault Suatton <thibault.suatton@ecl21.ec-lyon.fr>
2025-03-13 11:42:21 +0100

    Suppression des fichiers importés en trop

--- chargement_data.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df1 = pd.read_csv('Interconnections capacities.csv')
-#df2 = pd.read_csv('BDD.csv')
-
-pd.set_option('display.max_rows', None)
-pd.set_option('display.max_columns', None)
-
-print(df1)
