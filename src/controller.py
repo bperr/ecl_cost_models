67af3aa9d1a1fea8b63d775eebc414745ef29f7b
Thibault Suatton <thibault.suatton@ecl21.ec-lyon.fr>
2025-03-14 18:13:24 +0100

    Changement de certaines méthodes

--- src/controller.py
+++ src/controller.py
@@ -170,45 +170,36 @@ class Controller:
     
         :param results : dictionnary made by run with the computed 
         price model for each year range x zone x main sector.
-        :return : an xlsx sheet with the calculated values
         """
 
-        with pd.ExcelWriter('Output_prices.xlsx', engine='xlsxwriter') as writer:
-            for year, zones_data in results.items():
-                year_str = str(year)  
-        
-                data = []
-                all_sectors = set()  
-        
+        for year, zones_data in results.items():
+            year_str = str(year)  
+    
+            data = []
+            all_sectors = set()  
+    
+            
+            for zone_info in zones_data.values():
+                all_sectors.update(zone_info.keys())
+    
+            all_sectors = sorted(all_sectors)
+            columns = ['Zone', 'Price Type'] + list(all_sectors)
+    
+            for zone, sectors in zones_data.items():
                 
-                for zone_info in zones_data.values():
-                    for sector in zone_info.keys():
-                        all_sectors.add(sector)
-        
-                all_sectors = sorted(all_sectors)
-                columns = ['Zone', 'Price Type'] + list(all_sectors)
-        
-                for zone, sectors in zones_data.items():
-                    price_type_values = {
-                        'Cons_max': {sector: None for sector in all_sectors},
-                        'Cons_min': {sector: None for sector in all_sectors},
-                        'Prod_min': {sector: None for sector in all_sectors},
-                        'Prod_max': {sector: None for sector in all_sectors}
-                    }
-        
-        
-                    for sector, values in sectors.items():
-                        price_type_values['Cons_max'][sector] = values[0]
-                        price_type_values['Cons_min'][sector] = values[1]
-                        price_type_values['Prod_min'][sector] = values[2]
-                        price_type_values['Prod_max'][sector] = values[3]
-                        
-        
-                    for price_type, values in price_type_values.items():
-                        row = [zone, price_type] + [values[sect] for sect in all_sectors]
+                prices_type = ['Cons_max', 'Cons_min', 'Prod_min', 'Prod_max']
+                for zone, sectors_dict in zones_data.items():
+                    for index, price_type in enumerate(prices_type):
+                        row = [zone, price_type] + [sectors_dict.get(sect, [None] * 4)[index] for sect in all_sectors]
                         data.append(row)
-        
-                df = pd.DataFrame(data, columns=columns)
+    
+            df = pd.DataFrame(data, columns=columns)
+            
+            results_dir = self.work_dir / "results"
+            results_dir.mkdir(exist_ok=True)
+            
+            with pd.ExcelWriter(self.work_dir/"results"/"Output_prices.xlsx") as writer:
+                
                 df.to_excel(writer, sheet_name=year_str, index=False)
 
     def run(self, export_to_excel: bool) -> dict:
