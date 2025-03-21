154818d612483f763a3c5ed2bd341624f7972b7c
Tristan2LL <tristan.de-lacoste-lareymondie@etu.ec-lyon.fr>
2025-03-21 12:18:00 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,10 +2,13 @@ from pathlib import Path
 
 from controller import Controller
 
-studies_dir = Path(__file__).parents[1] / "studies"
-work_dir = studies_dir / "study_1"
-db_dir = studies_dir / "database"
+# studies_dir = Path(__file__).parents[1] / "studies"
+# work_dir = studies_dir / "study_1"
+# db_dir = studies_dir / "database"
+
+work_dir = Path(r"C:\Users\trist\OneDrive\Documents\ECL3A\Option énergie\Projet d'option\Code\database")
+db_dir = Path(r"C:\Users\trist\OneDrive\Documents\ECL3A\Option énergie\Projet d'option\Code\database")
 
 controller = Controller(work_dir=work_dir, db_dir=db_dir)
-results = controller.run(export_to_excel=False)  # FIXME Export to Excel
+results = controller.run(export_to_excel=True)  # FIXME Export to Excel
 print(results)
